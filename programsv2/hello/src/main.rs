55ade1d8338963782d3c2010b0e13eba18998afd
Edgar Luque <git@edgl.dev>
2025-10-05 13:56:41 +0200

    works

--- programsv2/hello/src/main.rs
+++ programsv2/hello/src/main.rs
@@ -1,17 +1,26 @@
 use std::{cell::Cell, fs::File, hint::black_box, io::Write, thread};
 
-
-
+thread_local! {
+    static HELLO: Cell<u64> = Cell::new(2);
+}
 
 fn main() {
-
     println!("{:?}", thread::current());
     println!("spawning..");
+    println!("m {:?}", HELLO.get());
+    HELLO.set(4);
+    println!("m {:?}", HELLO.get());
 
     thread::spawn(|| {
         println!("hello from thread {:?}", thread::current());
-    }).join();
+        println!("t {:?}", HELLO.get());
+        HELLO.set(6);
+        println!("t {:?}", HELLO.get());
+    })
+    .join()
+    .unwrap();
 
     println!("hello after joining");
-     println!("{:?}", thread::current());
+    println!("m {:?}", HELLO.get());
+    println!("{:?}", thread::current());
 }
